294cc4db1688c7edbbfcf0c76a6135d57bdaee4f
Nesanraaj Subramaniam <alternatenesan@gmail.com>
2022-11-08 15:01:47 +0800

    Removed some unnecessary comments

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup as sp 
-import requests
-import re
-import math
-import time
-import os
+import requests , re , math , time , os
 
 #attempt at obtaining url content
 #time in number of seconds until timeout
@@ -19,11 +15,7 @@ def download_content(url,time):
             time.sleep(0.25)
             continue
 
-#timeout for download_content() to obtain url, specified in seconds
-#sheet is an empty list
-#total is total number of jobs to scrape over
-#keyword for specific job, if no keyword specified all job listings will be scraped
-#path is directory to save textfiles in
+#refer readme file
 def scrape_job(timeout,sheet,total,substring_list,path,keyword=''):
     for pages in range(1,math.ceil(total/30)+1):
         if keyword == '':        
@@ -41,12 +33,13 @@ def scrape_job(timeout,sheet,total,substring_list,path,keyword=''):
         #find specific class that identifies the job listings
         job = soup.find('div',class_="sx2jih0 zcydq8bm").find('div',class_='sx2jih0')
         for jobs in job :
+                #obtain info
                 link_job = jobs.find('h1',class_='sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc3 _18qlyvca').a.get('href')
                 job_title = jobs.find('h1',class_='sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc3 _18qlyvca').a.text
                 pub = jobs.find('time',class_='sx2jih0 zcydq84u').text
                 url1 = 'https://www.jobstreet.com.my{}'.format(link_job)
                 #extract page
-                page1 = download_content(url1,3)
+                page1 = download_content(url1,timeout)
                 #parse with BS
                 soup1 = sp(page1,'html.parser')
                 #find specific class that identifies the job listings
@@ -69,17 +62,12 @@ def scrape_job(timeout,sheet,total,substring_list,path,keyword=''):
         if os.path.exists(prev_filename):
             os.remove(prev_filename)
         else:
-            print("The file does not exist")
+            print("First txt saved.")
         print('Completed page',(pages),'with',len(sheet),'candidates found.',(math.ceil(total/30)-pages),'page(s) left.')
-    
-#title of job (not required)
-keyword = 'data-analyst'
-#keywords to look for in description of each job
+
+keyword = 'data-analyst' #optional
 substring_list = ['fresh graduate','Fresh graduate','Fresh graduates','fresh graduates']
-sheet = []
-#obtain total manually to avoid long runtimes 
-total = 60
-#directory to save in
+sheet,total,timeout = [],60,3
 path = '/content/drive/MyDrive/'
 print("Started...")
-scrape_job(5,sheet,total,substring_list,path)
+scrape_job(timeout,sheet,total,substring_list,path)
